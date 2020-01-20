9b75b93ba5c7ec08b14b6537977d896c981d8f85
Rene Lochner <school@renelochner.at>
2020-01-20 20:38:44 +0100

    Implement "MainWindow"

--- source/ParkingTicketMachine.Wpf/MainWindow.xaml.cs
+++ source/ParkingTicketMachine.Wpf/MainWindow.xaml.cs
@@ -10,6 +10,9 @@ namespace ParkingTicketMachine.Wpf
     /// </summary>
     public partial class MainWindow
     {
+        public event EventHandler<SlotMachine> CreateNewSlotMachine;
+        public event EventHandler<Ticket> TicketEventHandler;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,12 +20,33 @@ namespace ParkingTicketMachine.Wpf
 
         private void Window_Initialized(object sender, EventArgs e)
         {
+            this.Title = SetWindowTitle();
+            OnCreateNewSlotMachine(new SlotMachine("Limesstrasse"));
+            OnCreateNewSlotMachine(new SlotMachine("Landstrasse"));
+        }
+
+        private string SetWindowTitle()
+        {
+            return $"Parkscheinzentrale, {FastClock.Instance.Time.ToString("HH:mm:ss")}";
         }
 
         private void ButtonNew_Click(object sender, RoutedEventArgs e)
         {
-            
+            OnCreateNewSlotMachine(new SlotMachine(TextBoxAddress.Text));
         }
 
+        protected virtual void OnCreateNewSlotMachine(SlotMachine slotMachine)
+        {
+            string title = slotMachine.Title;
+            if (title.Length > 0) {
+                CreateNewSlotMachine?.Invoke(this, slotMachine);
+                SlotMachineWindow window = new SlotMachineWindow(title, TicketEventHandler);
+                window.Owner = slotMachine;
+                window.Show();
+            } else
+            {
+                MessageBox.Show("Keine gültige Adresse.");
+            }
+        }
     }
 }
