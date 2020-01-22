4ea1ec85d916a74fc7b11f26526f89bf44bb3741
Rene Lochner <school@renelochner.at>
2020-01-22 19:06:52 +0100

    Implement "CalcTotalParkingTime" method

--- source/ParkingTicketMachine.Core/SlotMachine.cs
+++ source/ParkingTicketMachine.Core/SlotMachine.cs
@@ -21,6 +21,37 @@ namespace ParkingTicketMachine.Core
         {
             this.Title = title;
         }
+        public string CalcTotalParkingTime(int coin)
+        {
+            int minutes = 0;
+            DateTime time = DateTime.Now;
+
+            FastClock.Instance.IsRunning = false;
+
+            Sum += coin;
+
+            if(Sum >= MIN_COIN_INPUT && Sum < 100)
+            {
+                minutes = HALF_AN_HOUR_TARIFF;
+            }
+            else if(Sum >= 100 && Sum < 150)
+            {
+                minutes = 2 * HALF_AN_HOUR_TARIFF;
+            }
+            else if(Sum >= 150)
+            {
+                minutes = MAX_PARKING_TIME;
+            }
+
+            if(time >= FREE_PARKING_START_TIME && time <= FREE_PARKING_END_TIME)
+            {
+                ValidUntil = FastClock.Instance.Time.AddMinutes(minutes + FREE_PARKING_START_TIME.TimeOfDay.TotalMinutes);
+            }
+
+            ValidUntil = FastClock.Instance.Time.AddMinutes(minutes);
+
+            return ValidUntil.ToString("dd.MM.yyyy HH:mm");
+        }
         }
     }
 }
