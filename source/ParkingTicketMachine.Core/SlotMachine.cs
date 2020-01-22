27fec0f9377121e1ff0bd4aa319ef777e9becd58
Rene Lochner <school@renelochner.at>
2020-01-22 19:06:21 +0100

    Change properties in "SlotMachine"

--- source/ParkingTicketMachine.Core/SlotMachine.cs
+++ source/ParkingTicketMachine.Core/SlotMachine.cs
@@ -5,21 +5,22 @@ namespace ParkingTicketMachine.Core
 {
     public class SlotMachine
     {
+        public event EventHandler<Ticket> LogTicket;
+        private Ticket _ticket;
         public static readonly List<int> COIN_INSERT_OPTIONS = new List<int>() { 10, 20, 50, 100, 200 };
         public static readonly int MIN_COIN_INPUT = 50;
         public static readonly int HALF_AN_HOUR_TARIFF = 30;
         public static readonly int MAX_PARKING_TIME = 90;
-        public static readonly int FREE_PARKING_START_TIME = 18;
-        public static readonly int FREE_PARKING_END_TIME = 8;
+        public static readonly DateTime FREE_PARKING_START_TIME = DateTime.Parse("18:00");
+        public static readonly DateTime FREE_PARKING_END_TIME = DateTime.Parse("8:00");
 
         public string Title { get; }
-        public List<int> Coins { get; }
-        public DateTime ParkingDuration { get; set; }
-
+        public int Sum { get; private set; }
+        public DateTime ValidUntil { get; private set; }
         public SlotMachine(string title)
         {
             this.Title = title;
-            this.Coins = new List<int>();
+        }
         }
     }
 }
